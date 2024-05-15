f0e863a57b0df29f080701de723bc0288b33df3b
Trust_04zh <trust04zh@gmail.com>
2024-05-15 23:11:52 +0800

    init lab4

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![forbid(unsafe_code)]
+
+use aster_frame::prelude::*;
+
+#[aster_main]
+pub fn kern() {
+    println!("[Kernel] Greetings");
+    loop {}
+}
+
+#[cfg(ktest)]
+mod tests {
+    #[ktest]
+    fn it_works() {
+        let memory_regions = aster_frame::boot::memory_regions();
+        assert!(!memory_regions.is_empty());
+    }
+}
